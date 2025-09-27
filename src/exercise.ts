6d6ea65f004159ca9e18b13ba27fc8d70821836b
Ashahab007 <ashahab007@gmail.com>
2025-09-27 18:02:18 +0600

    fourth commit

---
+++ src/exercise.ts
@@ -0,0 +1,43 @@
+// student profile exercise
+
+type Student = {
+  name: string;
+  age: number;
+  isEnrolled: boolean;
+  courses: string[];
+  scores: number[];
+  info: [string, number, boolean];
+};
+
+const student1: Student = {
+  name: "Arham",
+  age: 7,
+  isEnrolled: true,
+  courses: ["English", "Bangla", "Math"],
+  scores: [90, 85, 98],
+  info: ["Arham", 7, true],
+};
+
+const student2: Student = {
+  name: "Afnan",
+  age: 6,
+  isEnrolled: false,
+  courses: [],
+  scores: [80, 75, 88],
+  info: ["Afnan", 6, false],
+};
+// now create a function to display student profile
+
+function getStudentInfo(student: Student): void {
+  console.log(`Name: ${student.name}`);
+  console.log(`Age: ${student.age}`);
+  console.log(`Course Enrolled: ${student.isEnrolled}`);
+  console.log(
+    `Courses Taken: ${
+      student.courses.length > 0 ? student.courses.join(", ") : 0
+    }`
+  );
+}
+
+getStudentInfo(student1);
+getStudentInfo(student2);
