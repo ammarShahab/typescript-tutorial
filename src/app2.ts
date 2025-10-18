1aa49436721873c4516f4f3f407c56f6ca7299cb
Ashahab007 <ashahab007@gmail.com>
2025-10-18 18:53:20 +0600

    commited

--- src/app2.ts
+++ src/app2.ts
@@ -13,6 +13,7 @@ function studInfo(user: { name: string; age: number }) {
 console.log(studInfo(student));
 
 //  4b. Type alias
+// you cannot reassign type alias
 // example:
 
 type Actor = {
@@ -47,3 +48,23 @@ function actorInfo(actor: Actor): string {
 }
 console.log(actorInfo(actor1));
 console.log(actorInfo(actor2));
+
+// 5. interface
+// interface is similar to type alias but it is used to define the structure of an object. it is also used to define the structure of a class. it is also used to define the structure of a function. you can extend or reassign an interface.
+
+// merge the interface of a person
+interface Person {
+  name: string;
+}
+
+interface Person {
+  //merge the interface of a person
+  age: number;
+}
+
+// but you can not merge type alias
+type Animal = {
+  name: string;
+};
+
+// type Animal = { age: number }; // error: Duplicate identifier 'Animal'.
