9feb4536c441d87c5b4aadea7d3eb5e347d74d8c
Ashahab007 <ashahab007@gmail.com>
2025-12-11 22:43:39 +0600

    updated

--- src/app2.ts
+++ src/app2.ts
@@ -101,6 +101,80 @@ const myDog: Dog = {
 };
 console.log("Dog info:", myDog);
 
+// a use case of type alias in mongodb database
+// define type with readonly where u cannot change the type of _id
+type Users = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  cardDetail?: creditCardDetail;
+};
+
+//at025._id="556565656" //unable to change the readonly data and shows error
+
+// adding creditCardDetails
+
+type creditCardNumber = {
+  cardNumber: number;
+};
+
+type creditCardExpiryDate = {
+  expiryDate: string;
+};
+
+type creditCardDetail = creditCardNumber &
+  creditCardExpiryDate & {
+    cvv: number;
+  };
+
+const cardDetail: creditCardDetail = {
+  cardNumber: 5624444,
+  expiryDate: "25/10/28",
+  cvv: 920,
+};
+
+const at025: Users = {
+  _id: "56447",
+  name: "Adnan",
+  email: "email@gmail.com",
+  isActive: false,
+  cardDetail: cardDetail,
+};
+
+console.log("at025", at025);
+
+// the upper method is not the best way to add credit card details but you should know the process.
+
+// In that case we can use interface
+interface Client {
+  readonly _id: string;
+  name: string;
+  age: number;
+  email: string;
+  isActive: boolean;
+}
+
+// you can also reassign interface
+interface Client {
+  cardNumber: number;
+  expiryDate: string;
+  cvv: number;
+}
+
+const at026: Client = {
+  _id: "54456666622222",
+  name: "Adnan",
+  age: 37,
+  email: "ashahab007@gmail.com",
+  isActive: true,
+  cardNumber: 654412222,
+  expiryDate: "25/09/29",
+  cvv: 920,
+};
+
+console.log("at026", at026);
+
 // when to use type alias and when to use interface?
 
 // i. designing public API use interface
