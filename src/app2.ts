746b7c386bfdbe16a21757a479f0890f2824e010
ammarShahab <ashahab007@gmail.com>
2026-08-23 22:55:33 +0600

    feat: add TypeScript tutorial exercises covering operators, types, interfaces, and advanced type patterns

--- src/app2.ts
+++ src/app2.ts
@@ -37,8 +37,8 @@ const modifiedUser: objType = {
 };
 console.log("spread operator in modified object: ", modifiedUser); //{ name: 'Akram', age: 40 }
 
-//! 5. Object type and type alias
-//! 5a. Object structure typing
+//! 6. Object type and type alias
+//! 6a. Object structure typing
 
 const student = {
   name: "Adnan Sami",
@@ -51,7 +51,7 @@ function studInfo(user: { name: string; age: number }) {
 
 console.log(studInfo(student));
 
-//! 5b. Type alias
+//! 6b. Type alias
 // type alias is used to define the structure of an object. It is also used to define the type of a variable that holds an object. you cannot reassign type alias.
 // example:
 
@@ -99,7 +99,7 @@ console.log(
 );
 console.log(actorInfo(actor2));
 
-//! 6. type interface
+//! 7. type interface
 // Interface is similar to type alias. But interface can be extended. It is used to define the structure of an object. It is also used to define the type of a variable that holds an object.
 interface Car {
   name: string;
